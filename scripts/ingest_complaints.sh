c65f02191b6d2880ab0b753fc399ce3f0f632262
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-12-07 15:38:23 -0600

    correcting script dir

--- scripts/ingest_complaints.sh
+++ scripts/ingest_complaints.sh
@@ -1,3 +1,3 @@
 # !usr/bin/bash
 
-cd ~/chi-pollution/complaint_data
+gcloud compute scp complaint_data/CDPH_Environmental_Complaints.csv cmmurray@mpcs53014c10-m-6-20191016152730:chi-pollution/complaint_data
