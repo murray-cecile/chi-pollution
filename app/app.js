13b8d25f23b43683d600c604862c3dbc0fa7d233
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-12-12 14:12:52 -0600

    format speed layer values

--- app/app.js
+++ app/app.js
@@ -1,15 +1,13 @@
 'use strict';
-// const http = require('http');
 var assert = require('assert');
 const express= require('express');
 const app = express();
 const mustache = require('mustache');
 const filesystem = require('fs');
-// const url = require('url');
 const hbase = require('hbase-rpc-client');
 const hostname = '127.0.0.1';
 const port = 3686;
-// const BigIntBuffer = require('bigint-buffer');
+const BigIntBuffer = require('bigint-buffer');
 
 var client = hbase({
     zookeeperHosts: ["mpcs53014c10-m-6-20191016152730.us-central1-a.c.mpcs53014-2019.internal:2181"],
@@ -22,29 +20,7 @@ client.on('error', function(err) {
 
 app.use(express.static('public'));
 
-// based on https://medium.com/@osiolabs/read-write-json-files-with-node-js-92d03cc82824
-filesystem.readFile('node_addresses.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("Error reading file from disk:", err)
-        return
-    }
-    try {
-        const nodes = JSON.parse(jsonString)
-        console.log("nodes") 
-} catch(err) {
-        console.log('Error parsing JSON string:', err)
-    }
-});
-
 app.get('/node-selection.html',function (req, res) { 
-
-	// // populate drop down menu dynamically?
-	// var dropdown = app.getElementById('address-dropdown');
-	// var option = app.createElement("option");
-	// option1.text("Cottage Grove Ave & 115th St Chicago IL");
-	// dropdown.add(option);
-	// option2.text("State St & Washington St Chicago IL");
-	// dropdown.add(option);
 
 	console.log(req.query["address"]);	
 	const address = req.query["address"];
@@ -96,36 +72,14 @@ app.get('/node-selection.html',function (req, res) {
 	html_data['num_noise_complaints'] = row.cols["complaints:noise_complaint"].value;
 	
 	if(row.cols["speed:last_seen"]) {
-		html_data['last_seen'] = row.cols["speed:last_seen"].value;
-		html_data['current_db'] = row.cols["speed:current_db"].value;
+		html_data['last_seen'] =  Date(row.cols["speed:last_seen"].value);
+		html_data['current_db'] = Number(BigIntBuffer.toBigIntBE(row.cols["speed:current_db"].value));
 	};
 
 	var template = filesystem.readFileSync("noise-result.mustache").toString();
 	var html = mustache.render(template, html_data)
 	res.send(html);
-	});
-
-	// query the current table
-	// const speed_get = new hbase.Get("07A");
-	// console.log(speed_get);
-
-	// client.get("cmmurray_hbase_node_names", speed_get, function(err, row) {
-	// 	assert.ok(!err, console.log(err));
-	// 	console.log("we got to 82 but idk how js works");
-	// 	if(!row){
-	// 		console.log("no row found");
-	// 		res.send("<html><body>No such node in data</body></html>");
-	// 		return;
-	// 	}
-
-	// // console.log(row.cols);
-
-	// // query this table for recent noise level
-	// html_data['current_db'] = row.cols['value'].value;
-
-	// });
-
-	
+	});	
 
 });
 
